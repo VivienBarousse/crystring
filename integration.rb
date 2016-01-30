78bb4ba6209c0a21a86d15de0f2100e793a635be
Vivien Barousse <barousse.vivien@gmail.com>
2016-01-30 13:28:10 +0000

    Add some sort of integration tests

---
+++ integration.rb
@@ -0,0 +1,11 @@
+
+def test_program(name, expected_output)
+  program = File.expand_path('../' + name, __FILE__)
+  actual_output = `bin/crystring #{program}`
+  unless actual_output == expected_output
+    raise "Program #{name} failed, expected #{expected_output.inspect}, got #{actual_output.inspect}"
+  end
+end
+
+test_program("examples/hello_world.str", "Hello world!\n")
+
