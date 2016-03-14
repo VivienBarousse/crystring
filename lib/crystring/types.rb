0cb24689a3f81a72f0c6f262ae24feb8c405815f
Vivien Barousse <barousse.vivien@gmail.com>
2016-03-14 18:27:31 +0000

    Introduce base type to avoid mindless duplication

--- lib/crystring/types.rb
+++ lib/crystring/types.rb
@@ -1,5 +1,6 @@
 module Crystring
   module Types
+    autoload :Base, 'crystring/types/base'
     autoload :String, 'crystring/types/string'
     autoload :Integer, 'crystring/types/integer'
   end
---
+++ lib/crystring/types/base.rb
@@ -0,0 +1,55 @@
+module Crystring
+  module Types
+    class Base
+      def initialize(str)
+        @str = str
+        @variables = {}
+      end
+
+      def call_method(name, args)
+        raise "Unknown method #{name}" unless self.class.get_method(name)
+        self.class.get_method(name).invoke(args)
+      end
+
+      def self.call_method(name, args)
+        raise "Unknown method #{name}" unless name == "new"
+        raise "Invalid number of arguments #{args.count}, expected 1" unless args.count == 1
+        new(args.first.to_s)
+      end
+
+      def self.def_method(name, function)
+        @methods ||= {}
+        @methods[name] = function
+      end
+
+      def self.get_method(name)
+        @methods ||= {}
+        @methods[name]
+      end
+
+      def to_s
+        @str
+      end
+
+      def ==(other)
+        self.to_s == other.to_s
+      end
+
+      def variable_exists?(name)
+        return true if name == "self"
+        @variables.has_key?(name)
+      end
+
+      def get_variable(name)
+        return @str if name == "self"
+        @variables[name]
+      end
+
+      def set_variable(name, value)
+        raise "Can't set `self`" if name == str
+        @variables[name] = value
+      end
+    end
+  end
+end
+
